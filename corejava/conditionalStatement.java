fb277893f640c18808f824df02121f429852fd75
kavyavr.xworkz@gmail.com <kavyavr.xworkz@gmail.com>
2022-03-21 12:02:24 +0530

    adding

---
+++ corejava/conditionalStatement.java
@@ -0,0 +1,14 @@
+public class conditionalStatement {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+
+        if(a > b) {
+            System.out.println("a is greater");
+        } else if(a < b) {
+            System.out.println("b is greater");
+        } else {
+            System.out.println("both are equal");
+        }
+    }
+}
